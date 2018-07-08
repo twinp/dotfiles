3ff116d8eded3119c84c5eb6dd9d7537d241bdd8
Romain <romain.basseville@gmail.com>
2018-07-08 16:00:07 +0800

    Clean init.el and automate package management

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,32 +1,7 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
- '(frame-background-mode (quote dark))
- '(initial-buffer-choice (quote org-agenda-list))
- '(menu-bar-mode nil)
- '(ns-auto-hide-menu-bar t)
- '(org-agenda-files
-   (quote
-    ("~/Dropbox/Orgzly/work.org" "~/Dropbox/Orgzly/explore-new.org" "~/Dropbox/Orgzly/perso.org" "~/Dropbox/Orgzly/geek.org" "~/Dropbox/Orgzly/finance.org")))
- '(package-archives
-   (quote
-    (("gnu" . "https://elpa.gnu.org/packages/")
-     ("melpa" . "http://melpa.org/packages/"))))
- '(package-selected-packages
-   (quote
-    (org-pomodoro color-theme color-theme-solarized go-guru go-mode magit org))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
